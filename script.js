a7d6e7990b1e066447f93e9a662146e517b2289d
sabamgeliashvili <sba69717@gmail.com>
2022-07-28 21:03:29 +0400

    .

---
+++ script.js
@@ -0,0 +1,66 @@
+// TASK1
+
+
+let  user = {
+    firstname: 'ana',
+     age: 27,
+   };
+
+   let  user2 = {
+    firstname: 'levani',
+     age: 21,
+   };
+   console.log('ana giorgize 7 wlit ufrosia');
+   let checkUsertype = typeof user;
+   console.log(typeof checkUsertype)
+   
+
+// TASK 2
+
+let names = ['John','Sam','Ann','Will','Andrew','Joseph','Lorelai','Amelie','Den']
+let middleElement = names[4]
+console.log(middleElement)
+
+// TASK 3
+
+let student = {
+firstname:"ann",
+lastname:"smith",
+age: 22,
+subjects: ['math','english','biology','geography','history'],
+roommate: {
+fullname: 'helen jackson',
+age: 19,
+},
+};
+for (let i = 0; i++;); {
+    console.log([student.subjects]);
+   }
+let fullname = {
+    roommate1: {
+        fullname: 'ann smith'
+    },
+    roommate2: {
+        fullname: 'helen jackson'
+    },
+};
+
+// // TASK 4
+
+let objects = ["Banana", "Orange", "Apple", "Mango",2,12]
+let i = 0;
+while (i < 4) {
+    console.log(objects[i]);
+    i++;
+}
+
+
+// // // TASK 5
+
+
+let numbers = [12,23,43,11,9,2,121,90];
+if (numbers > 31, numbers % 2 == 0); {
+    console.log('Element is greater than provided value and is EVEN');
+} if (numbers < 31, numbers % 2 == 1); { 
+    console.log('Element is less than provided value and is ODD');
+}
